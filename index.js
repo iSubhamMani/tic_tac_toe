5fa1a01a5538be8973922c323c96e142e140d918
Subham Mani <manisubham09@gmail.com>
2023-08-22 15:48:49 +0530

    add grid listeners

--- index.js
+++ index.js
@@ -10,12 +10,39 @@ const GameBoard = (() => {
 
 const DisplayController = (() => {
     // display signatures on grid
-    const gridCells = document.querySelectorAll('.grid-cell');
     const gameBoardItems = GameBoard.getGameBoard();
 
-    gridCells.forEach( (gridCell, index) => {
-        gridCell.textContent = gameBoardItems[index];
-    })
+    const updateGameBoard = (gridCells) => {
+        gridCells.forEach( (gridCell, index) => {
+            gridCell.textContent = gameBoardItems[index];
+        });
+    }
+
+    return {updateGameBoard};
+})();
+
+const GameController = (() => {
+
+    const gridCells = document.querySelectorAll('.grid-cell');
+
+    const updateGameBoard = (gridCell) => {
+        const index = parseInt(gridCell.getAttribute('data-index'));
+        GameBoard.getGameBoard()[index] = 'X';
+
+        DisplayController.updateGameBoard(gridCells);
+    }
+
+    const addGridListener = () => {
+        gridCells.forEach(gridCell => {
+            gridCell.addEventListener('click', () => {
+                if(gridCell.textContent === ''){
+                    updateGameBoard(gridCell);
+                }
+            })
+        })
+    };
+
+    addGridListener();
 })();
 
 const Player = (signature) => {
